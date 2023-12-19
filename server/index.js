c0ab9e2938a4442afa69a913d4b29243c65adeaa
Matěj Pešl <matejpesl1@gmail.com>
2023-12-19 13:58:54 +0100

    implement sentiment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import { WebSocketServer } from "ws";
 import express from 'express';
 import http from 'http';
+import OpenAI from 'openai';
+import { v4 as uuidv4 } from 'uuid';
+
+// Make sure to set your OpenAI API key in an environment variable for security
+const openai = new OpenAI({
+  apiKey: "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",
+});
 
 const app = express();
 const server = http.createServer(app);
@@ -9,25 +16,58 @@ const wss = new WebSocketServer({ server });
 wss.on('connection', function connection(ws) {
   console.log('A new client connected!');
 
-  ws.on('message', function incoming(message) {
+  ws.on('message', async function incoming(data) {
+    const message = JSON.parse(data);
     console.log('received: %s', message);
 
-    // Broadcast to everyone else.
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
+    // Assign an ID to each message
+    const messageId = uuidv4();
+    message.id = messageId;
 
-  ws.on('close', () => {
-    console.log('Client disconnected');
+    // Broadcast the message to everyone else.
+    wss.clients.forEach(function each(client) {
+      client.send(JSON.stringify(message));
+    });
+
+    // Perform sentiment analysis
+    try {
+      const sentiment = await getSentiment(message.message);
+      console.log("sentiment: ", sentiment);
+
+      // Broadcast the sentiment result to all clients with the message ID
+      const sentimentMessage = { id: messageId, sentiment: sentiment.trim() };
+      wss.clients.forEach(function each(client) {
+        client.send(JSON.stringify(sentimentMessage));
+      });
+    } catch (error) {
+      console.error('Error performing sentiment analysis:', error);
+    }
   });
 });
 
+async function getSentiment(message) {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: 'You are an expert in categorizing sentiment of a message. ' +
+          'You only return either "positive", "negative", "profane", or "neutral"'
+      },
+      {
+        role: 'user',
+        content: 'Categorize the sentiment of the following message (return output only one word): ' + message
+      }
+    ],
+    temperature: 0.3,
+  });
+
+  return response.choices[0].message.content;
+}
+
 // Serve any static files
-app.use(express.static('path-to-your-react-app-build'));
+// app.use(express.static('path-to-your-react-app-build'));
 
 server.listen(3001, function listening() {
-  console.log('Listening on %d', server.address().port);
+  console.log('Listening on 3001');
 });
