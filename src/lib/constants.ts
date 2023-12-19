22bf5e06c878abb0c72aaf0eb4e885b149d3e29a
Matěj Pešl <matejpesl1@gmail.com>
2023-12-19 21:06:38 +0100

    fix ws url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
   serverUrl: import.meta.env.DEV ? 'http://localhost:3001' : 'https://hackathonussy.up.railway.app',
   // Websocket needs a different protocol (ws instead of localhost).
-  wsServerUrl: import.meta.env.DEV ? 'ws://localhost:3001/ws' : 'wss://hackathonussy.up.railway.app',
+  wsServerUrl: import.meta.env.DEV ? 'ws://localhost:3001/ws' : 'wss://hackathonussy.up.railway.app/ws',
 }
